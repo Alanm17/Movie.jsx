65091dd64461ff84f834139c9a73b53c461edf07
Alan <Alan2003@sju.ac.kr>
2025-01-03 01:40:07 +0900

    props as componentAPI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={5} onSetRating={setMovieRating} />
+      <p>This movie is rated {movieRating} stars</p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
 
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} defaultRating={5} onSetRating={() => {}} />
+    <StarRating
+      maxRating={5}
+      size={20}
+      color="orange"
+      className={""}
+      messages={["terrible", "bad", "okey", "good", "amazing"]}
+    />
+    <Test />
   </React.StrictMode>
 );
